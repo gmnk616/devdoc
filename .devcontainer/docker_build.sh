a43fc2d64e3d73c99057b97101a000761829ee38
gmnk <maqt01476497@gmail.com>
2025-02-18 22:32:30 +0900

    - `.devcontainer/docker_build.sh`を修正しました。 - `.vscode`を追加しました。 - `.editorconfig`を追加しました。

--- .devcontainer/docker_build.sh
+++ .devcontainer/docker_build.sh
@@ -4,7 +4,30 @@ set -e
 
 # dockerイメージ名
 IMAGE=gmnk/devdoc-dev
+# docker build時にキャッシュを使用しない場合は`--no-cache`が入る
+NO_USE_CACHE=""
 
-# shellcheck disable=SC1091
-. "${HOME}"/wslenv/docker_build_push.sh
-docker_build_push "$@" -i ${IMAGE}
+# コマンドオプションで引数処理(引数は順不同)
+while getopts "c" option
+do
+  case $option in
+    c)
+      NO_USE_CACHE="--no-cache"
+      ;;
+    \?)
+      echo "param err" 1>&2
+      exit 1
+      ;;
+  esac
+done
+
+# HTTPプロキシの値
+FIX_PROXY_VAL=""
+# dockerタグ名
+TAG="latest"
+
+# dockerイメージ作成
+docker image build ${NO_USE_CACHE} \
+  --build-arg http_proxy="${FIX_PROXY_VAL}" \
+  --build-arg https_proxy="${FIX_PROXY_VAL}" \
+  -t "${IMAGE}":"${TAG}" .
